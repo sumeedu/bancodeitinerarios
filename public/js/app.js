9365949c4b0751fc8fa0b846755d3c7bd7e4c4ce
Ricardo Lüders <rluders@redhat.com>
2022-03-03 20:45:53 +0100

    tuning search bar

--- public/js/app.js
+++ public/js/app.js
@@ -6452,12 +6452,12 @@ var Authenticated = function Authenticated(_ref) {
     className: "input-group relative flex items-stretch w-full"
   }, react_1["default"].createElement("input", {
     type: "search",
-    className: "form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none",
+    className: "form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded rounded-r-none transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none",
     placeholder: "Search",
     "aria-label": "Search",
     "aria-describedby": "button-addon2"
   }), react_1["default"].createElement("button", {
-    className: "btn px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center",
+    className: "btn px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded rounded-l-none shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center",
     type: "button",
     id: "button-addon2"
   }, react_1["default"].createElement("svg", {
