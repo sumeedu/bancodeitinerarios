3d8ad500b95494035629ca89c808bd45bf7f9ec3
Ricardo Lüders <rluders@redhat.com>
2022-03-03 09:32:55 +0100

    basic review box

---
+++ resources/js/Components/ReviewBox.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+const ReviewBox: React.FC = (props) => {
+  return (
+    <div className="mx-auto py-4 max-w-sm ">
+      <div className="mb-1 tracking-wide py-4">
+        <h2 className="text-gray-800 font-semibold mt-1">67 reviews</h2>
+        <div className="border-b pb-3">
+          <div className="flex items-center mt-1">
+            <div className=" w-1/5 text-indigo-500 tracking-tighter">
+              <span>5 stars</span>
+            </div>
+            <div className="w-3/5">
+              <div className="bg-gray-300 w-full rounded-lg h-2">
+                <div className=" w-7/12 bg-indigo-600 rounded-lg h-2"></div>
+              </div>
+            </div>
+            <div className="w-1/5 text-gray-700 pl-3">
+              <span className="text-sm">51%</span>
+            </div>
+          </div>
+          <div className="flex items-center mt-1">
+            <div className="w-1/5 text-indigo-500 tracking-tighter">
+              <span>4 stars</span>
+            </div>
+            <div className="w-3/5">
+              <div className="bg-gray-300 w-full rounded-lg h-2">
+                <div className="w-1/5 bg-indigo-600 rounded-lg h-2"></div>
+              </div>
+            </div>
+            <div className="w-1/5 text-gray-700 pl-3">
+              <span className="text-sm">17%</span>
+            </div>
+          </div>
+          <div className="flex items-center mt-1">
+            <div className="w-1/5 text-indigo-500 tracking-tighter">
+              <span>3 stars</span>
+            </div>
+            <div className="w-3/5">
+              <div className="bg-gray-300 w-full rounded-lg h-2">
+                <div className=" w-3/12 bg-indigo-600 rounded-lg h-2"></div>
+              </div>
+            </div>
+            <div className="w-1/5 text-gray-700 pl-3">
+              <span className="text-sm">19%</span>
+            </div>
+          </div>
+          <div className="flex items-center mt-1">
+            <div className=" w-1/5 text-indigo-500 tracking-tighter">
+              <span>2 stars</span>
+            </div>
+            <div className="w-3/5">
+              <div className="bg-gray-300 w-full rounded-lg h-2">
+                <div className=" w-1/5 bg-indigo-600 rounded-lg h-2"></div>
+              </div>
+            </div>
+            <div className="w-1/5 text-gray-700 pl-3">
+              <span className="text-sm">8%</span>
+            </div>
+          </div>
+          <div className="flex items-center mt-1">
+            <div className="w-1/5 text-indigo-500 tracking-tighter">
+              <span>1 stars</span>
+            </div>
+            <div className="w-3/5">
+              <div className="bg-gray-300 w-full rounded-lg h-2">
+                <div className=" w-2/12 bg-indigo-600 rounded-lg h-2"></div>
+              </div>
+            </div>
+            <div className="w-1/5 text-gray-700 pl-3">
+              <span className="text-sm">5%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full px-4">
+        <h3 className="font-medium tracking-tight">Faça um review</h3>
+        <p className="text-gray-700 text-sm py-1">
+          e dê seu feedback sobre esse itinerário.
+        </p>
+        <button className="bg-gray-100 border border-gray-400 px-3 py-1 rounded  text-gray-800 mt-2">
+          Fazer um review
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ReviewBox
+
+
--- resources/js/Pages/Itinerary/Show.tsx
+++ resources/js/Pages/Itinerary/Show.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import Authenticated from "@/Layouts/Authenticated";
-import {Head} from "@inertiajs/inertia-react";
-import Container from "@/Components/Container";
-import ObjectiveIcon from "@/Components/ObjectiveIcon";
+import Authenticated from "@/Layouts/Authenticated"
+import {Head} from "@inertiajs/inertia-react"
+import Container from "@/Components/Container"
+import ObjectiveIcon from "@/Components/ObjectiveIcon"
+import moment from 'moment'
+import ReviewBox from "@/Components/ReviewBox";
 
 const Index: React.FC = (props: React.PropsWithChildren<any>) => {
   const {itinerary} = props
@@ -30,7 +32,21 @@ const Index: React.FC = (props: React.PropsWithChildren<any>) => {
           <div className="col-span-2 p-6">
             <div className="mb-6 border-b pb-6">
               <h3 className="text-gray-900 text-xl font-medium mb-2 text-xl">{itinerary.name}</h3>
-              <p className="text-gray-600 text-xs">Criado por: {itinerary.user.name}</p>
+
+              <div className="flex flex-row mt-2 py-3 ">
+                <div className="w-auto h-auto rounded-full border-2">
+                  <img className='w-12 h-12 object-cover rounded-full shadow' alt='User avatar'
+                       src='https://i.pravatar.cc/200' />
+                </div>
+                <div className="flex flex-col mb-2 ml-4 mt-1">
+                  <div className='text-gray-600 text-sm font-semibold'>{itinerary.user.name}</div>
+                  <div className='flex w-full mt-1'>
+                    <div className='text-gray-400 font-thin text-xs'>
+                      Atualizado a {moment(itinerary.updated_at).fromNow()}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
             <h4 className="font-semibold mt-4 mb-2 text-lg">Descrição</h4>
@@ -50,6 +66,7 @@ const Index: React.FC = (props: React.PropsWithChildren<any>) => {
           </div>
 
           <div className="bg-gray-50 p-6 rounded-tr-lg">
+
             <h4 className="font-semibold mt-4 mb-2 text-lg">Áreas do Conhecimento</h4>
             <p>{areas.join(', ')}</p>
 
@@ -62,6 +79,11 @@ const Index: React.FC = (props: React.PropsWithChildren<any>) => {
                 <ObjectiveIcon name={n} />
               ))}
             </div>
+
+            <div className="mt-4">
+              <ReviewBox />
+            </div>
+
           </div>
 
         </div>
