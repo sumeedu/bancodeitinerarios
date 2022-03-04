5c78484f678af4227373e0eed8a991b482805163
Ricardo Lüders <rluders@redhat.com>
2022-03-04 16:55:47 +0100

    redirect and remove welcome

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,16 @@ use Inertia\Inertia;
 
 require __DIR__.'/auth.php';
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//});
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
 
